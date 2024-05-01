45da37b3a51ada5879e0c77bdc13f7cdb3a8c5aa
Sophia Krugler <krugler@usc.edu>
2024-05-01 18:31:25 +0100

    Comment the diceroller and gamehud scripts.

--- BasicMapTest2/Assets/Scripts/GameScripts/DiceRollerScript.cs
+++ BasicMapTest2/Assets/Scripts/GameScripts/DiceRollerScript.cs
@@ -1,17 +1,15 @@
 using UnityEngine;
-using System;
 
+
+/// <summary>
+/// Utility class to create die rolls. 
+/// </summary>
 public class DiceRollerScript : MonoBehaviour
 {
 
     // Method to be called when we want to roll the dice
     public int RollDice()
     {
-        return UnityEngine.Random.Range(1, 7); // Random dice roll between 1 and 6
-    }
-
-    // Update is used to check for input to roll the dice
-    private void Update()
-    {
+        return Random.Range(1, 7); // Random dice roll between 1 and 6
     }
 }
