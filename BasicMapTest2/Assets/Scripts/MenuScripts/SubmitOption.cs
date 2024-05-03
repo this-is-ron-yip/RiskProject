003e0fb2b83c9c6218f982096c3c5101b260a4cb
CrystalChan <94296148+CrystalChann@users.noreply.github.com>
2024-05-03 12:12:19 +0100

    Fixed compile bugs by deleting Callbacks

--- BasicMapTest2/Assets/Scripts/MenuScripts/SubmitOption.cs
+++ BasicMapTest2/Assets/Scripts/MenuScripts/SubmitOption.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Callbacks;
 using System;
 
 public class SubmitOption : MonoBehaviour
